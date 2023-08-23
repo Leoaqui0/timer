63cbf934586b91373082cfdcbe1d1027426883fb
Leozin <110321272+Leoaqui0@users.noreply.github.com>
2023-08-23 19:53:46 -0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    question: "Qual a fórmula para calcular a área de um triângulo?",
+    answers: [
+      { option: " A = l x l ", correct: false },
+      { option: "A = b x h ", correct: false },
+      { option: "A = (b x h)/2 ", correct: true },
+      { option: "A = π x r²", correct: false},
+    ],
+  },
+  {
+    question: "10 - 6 x 2?",
+    answers: [
+      { option: "2", correct: false },
+      { option: "-2", correct: true },
+      { option: "-12", correct: false },
+      { option: "12", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o resultado de 17 + 8 - 3?",
+    answers: [
+      { option: "12", correct: false},
+      { option: "22", correct: true },
+      { option: "32", correct: false },
+      { option: "46", correct: false },
+    ],
+  },
+  {
+    question: "Dois pais e dois filhos sentaram-se para comer ovos no café da manhã. Cada um comeu um ovo. Quanto os ovos eles comeram no total?",
+    answers: [
+      { option: "1", correct: false },
+      { option: "3", correct: true },
+      { option: "5", correct: false },
+      { option: "8", correct: false},
+    ],
+  },
+  {
+    question: " Andando por uma rua, um homem conta 10 árvores à sua direita. Na volta, conta 10 árvores à sua esquerda. Quantas árvores ele viu no total?",
+    answers: [
+        { option: "9", correct: true},
+        { option: "10", correct: false },
+        { option: "11", correct: false },
+        { option: "11", correct: false },
+    ],
+  },
+];
+  
+
+
+
